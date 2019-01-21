f1520d2576e562f7d9c67bb6404a06c960fc53bd
Greg Morgan <gregm9999@gmail.com>
2019-01-21 23:47:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-
-function loadIssue() {
-  var issue = {
-    state: "closed",
-    number: 5,
-    created_at: "2016-03-31 16:23:13 UTC",
-    body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
-  }
- 
+function loadIssues() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  var result = template(issue);
+  var result = template(issues);
   document.getElementsByTagName("main")[0].innerHTML += result;
 }
